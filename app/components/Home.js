2daf76bc76e2d06a8357a3b5b99c3bd1f444a68a
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-06-26 17:37:57 +0800

    Add http-server for heroku. Make some changes to home.

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,8 +4,12 @@ var Link = require('react-router-dom').Link;
 function Home() {
   return (
     <div className='home-container'>
-      <h1>Battle your friendzzz</h1>
+      <h1>Battle yo friendzzz</h1>
       <Link className='button' to='/battle'>Battle</Link>
+      <br/>
+      <p>
+        Note: this is from a tutorial in <a href="https://online.reacttraining.com">online.reacttraining.com</a>. The original web app can be found <a href="https://rt-react-fundamentals.firebaseapp.com/">here</a>. -carl
+      </p>
     </div>
   )
 }
