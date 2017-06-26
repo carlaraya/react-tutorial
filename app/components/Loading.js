e8e32e0ac7c138657f0e7473321b0e961f552163
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-06-26 16:31:00 +0800

    Composition via props.children && Building a Highly Reusable React Component (sorry its 2)

---
+++ app/components/Loading.js
@@ -0,0 +1,48 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+};
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: props.text
+    }
+  }
+  componentDidMount() {
+    var stopper = this.props.text + '...';
+    this.interval = window.setInterval(function() {
+      this.setState(function(prevState) {
+        return {
+          text: this.state.text === stopper ? this.props.text : prevState.text + '.'
+          }
+      });
+    }.bind(this), this.props.speed);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 300
+}
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+}
+
+module.exports = Loading;
