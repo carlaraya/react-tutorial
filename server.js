2ecaed3d098a98487ab879a33e01e320f4ff4ac8
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-07-07 19:18:27 +0800

    Fix refresh route issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -7,6 +8,10 @@ app.use(express.static(__dirname + '/dist'))
 app.get('/api', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at " + app.get('port'))
